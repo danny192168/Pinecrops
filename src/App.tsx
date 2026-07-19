148bef0d33fcadcc78cfa2f974a944cf04bb7b06
DannyBase2025 <dannybase2025@gmail.com>
2026-07-19 14:16:32 +0800

    vercel fix try

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Product } from "./pages/Product";
 function App() {
   return (
     <div>
-      <Router basename={import.meta.env.BASE_URL}>
+      <Router>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/" element={<Listings />} />
