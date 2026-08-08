709e8a49f7a3aa9ece58c8690f0e6106e42cd11a
danny192168 <danny192168@gmail.com>
2026-08-09 01:44:54 +0800

    vercel fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ import babel from "@rolldown/plugin-babel";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
-  base: process.env.VITE_BASE_PATH || "/Pinecrops",
+  // base: process.env.VITE_BASE_PATH || "/Pinecrops",
 
   // uncomment this and swap to one above when ready to deploy on vercel:
-  // base: '/',
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
